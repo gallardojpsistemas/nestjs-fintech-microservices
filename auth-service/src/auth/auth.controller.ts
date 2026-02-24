a7e914bfe0cf7b8d23cef348f6b1f73c6ac6caef
gallardojpsistemas <gallardojpsistemas@gmail.com>
2026-02-24 15:23:17 -0300

    feat: implement jwt logic

--- auth-service/src/auth/auth.controller.ts
+++ auth-service/src/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { AuthService } from './auth.service';
+import { JwtAuthGuard } from './jwt/jwt-auth.guard';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('me')
+    getProfile(@Request() req: Request & { user: JwtPayload }) {
+        return req.user;
+    }
 
     @Post('login')
     async login(@Body() body: RegisterDto) {
--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt/jwt.strategy';
 
 @Module({
   imports: [
@@ -13,6 +14,6 @@ import { JwtModule } from '@nestjs/jwt';
     })
   ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule { }

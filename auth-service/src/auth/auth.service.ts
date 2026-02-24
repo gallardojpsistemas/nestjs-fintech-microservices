a196eb50d24a53ed52bf30134848a2288a3e8c79
gallardojpsistemas <gallardojpsistemas@gmail.com>
2026-02-24 14:30:21 -0300

    feat: add register logic

--- auth-service/src/auth/auth.service.ts
+++ auth-service/src/auth/auth.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private readonly usersService: UsersService) { }
+
+    async register(email: string, password: string) {
+        const existingUser = await this.usersService.findByEmail(email);
+
+        if (existingUser)
+            throw new BadRequestException('User already exists');
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await this.usersService.create(email, hashedPassword);
+
+        return {
+            id: user._id.toString(),
+            email: user.email,
+        };
+    }
+}
--- auth-service/src/users/users.service.ts
+++ auth-service/src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel(User.name)
+        private readonly userModel: Model<UserDocument>,
+    ) { }
+
+    async create(email: string, password: string): Promise<UserDocument> {
+        const user = new this.userModel({ email, password });
+
+        return user.save();
+    }
+
+    async findByEmail(email: string): Promise<UserDocument | null> {
+        return this.userModel.findOne({ email });
+    }
+}

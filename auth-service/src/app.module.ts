2cc28010a317318145b2a3bc6b5d7c72e6609e4c
gallardojpsistemas <gallardojpsistemas@gmail.com>
2026-02-24 13:25:12 -0300

    feat: connect mongodb

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/fintech-auth'),
+    AuthModule,
+    UsersModule
+  ],
 })
 export class AppModule { }

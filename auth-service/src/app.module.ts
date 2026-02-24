a3e4dfea0c220e31f5e97d05201404b1bafd77da
gallardojpsistemas <gallardojpsistemas@gmail.com>
2026-02-24 20:47:13 -0300

    feat: add config logic

--- auth-service/src/app.module.ts
+++ auth-service/src/app.module.ts
@@ -2,10 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/fintech-auth'),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+    }),
     AuthModule,
     UsersModule
   ],
--- auth-service/src/auth/auth.module.ts
+++ auth-service/src/auth/auth.module.ts
@@ -5,13 +5,21 @@ import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt/jwt.strategy';
 import { RolesGuard } from './guards/roles.guard';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { StringValue } from 'ms';
 
 @Module({
   imports: [
     UsersModule,
-    JwtModule.register({
-      secret: 'super-secret-key',
-      signOptions: { expiresIn: '1h' },
+    ConfigModule,
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<string>('JWT_EXPIRES_IN') as StringValue,
+        },
+      }),
     })
   ],
   controllers: [AuthController],
